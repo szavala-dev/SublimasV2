e61a03015ac020c46fec960c90658943ef541adf
sebastianzavala <sebastian.zavala@gmail.com>
2023-12-11 17:16:40 -0300

    Crud de marca

--- SistemaSublimas/Areas/Admin/Controllers/MarcaController.cs
+++ SistemaSublimas/Areas/Admin/Controllers/MarcaController.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using SistemaSublimas.AccesoDatos.Repositorio.IRepositorio;
 using SistemaSublimas.Modelos;
 using SistemaSublimas.Utilidades;
